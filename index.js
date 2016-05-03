a58d8129e28bc12660b92b0fc7119501676541af
Taras Kolodny <trskldn@gmail.com>
2016-05-03 16:22:40 +0300

    error message

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ module.exports = function(price, cash){
 		change = cash - price,
 		coin;
 
-	if (isNaN(change)) throw new TypeError('invalid input value'); 
-	if (change < 0) throw new Error('not enough cash'); 
-	if (price < 0 || cash < 0) throw new Error('expected positive number');
+	if (isNaN(change)) throw new TypeError('price and cash should be a number'); 
+	if (change < 0) throw new Error('cash should be greater than price'); 
+	if (price < 0) throw new Error('price should be positive number');
+	if (cash < 0) throw new Error('cash should be positive number');
+  
 	while (change > 0) {
 		 coin = COINS.find(item => change >= item);
 		 change -= coin;
