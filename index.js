19224f91b91cad956ef8c2da2e4ec352b40f9733
Taras Kolodny <trskldn@gmail.com>
2016-05-03 14:44:50 +0300

    add test, and implement functionals

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ var COINS = [200, 100, 50, 20, 10, 5, 2, 1];
 
 module.exports = function(price, cash){
 	var rez = [],
-		change = cash - price;
+		change = cash - price,
+		coin;
 
 	if (isNaN(change)) throw new TypeError('invalid input value'); 
 	if (change < 0) throw new Error('not enough cash'); 
-	if(change === 0) return [];
+	if (price < 0 || cash < 0) throw new Error('expected positive number');
+	while (change > 0) {
+		 coin = COINS.find(item => change >= item);
+		 change -= coin;
+		 rez.push(coin);
+	}
 
-
- 	return [];
+ 	return rez;
 };
--- test/test-coins-change.js
+++ test/test-coins-change.js
@@ -4,24 +4,45 @@ var expect = require('chai').expect;
 process.env.NODE_ENV = 'test';
 
  describe('getChange', function() {
- 	it('should throw TypeError,  when input value not present', function() {
- 		expect(getChange).to.throw(TypeError);
+
+ 	describe('result', function() {
+	 	it('getChange(13, 50) should return  [20, 10, 5, 2]', function() {
+			var result = getChange(13, 50);
+	 		expect(result).to.be.deep.equal([20, 10, 5, 2]);   
+	 	});
+
+	 	it('when price equal to paid cash ,should return empty array', function() {
+	 		var result = getChange(100,100);
+	 		expect(result).to.be.deep.equal([]);
+	 	});
+
+	 	it('should throw error,when price bigger then paid cash', function() {
+	 		expect(getChange.bind(null, 50, 13)).to.throw(Error);   
+	 	});
+
  	});
 
- 	it('getChange(13, 50) should return  [20, 10, 5, 2]', function() {
-		var result = getChange(13, 50);
- 		expect(result).to.be.deep.equal([20, 10, 5, 2]);   
- 	});
- 
- 	it('should throw error,when price bigger then cache', function() {
- 		expect(getChange.bind(null, 50, 13)).to.throw(Error);   
- 	});
- 	
- 	it('when price equal to paid cash ,should return empty array', function() {
- 		var result = getChange(100,100);
- 		expect(result).to.be.deep.equal([]);
- 	});
+ 	describe('arguments', function() {
+	 	it('getChange() should throw TypeError,  when input value not present', function() {
+	 		expect(getChange).to.throw(TypeError);
+	 	});
 
+	 	it('getChange(-1, -1) should throw Error', function() {
+			expect(() => getChange(-1, -1) ).to.throw(Error);
+	 	});
+
+	 	it('getChange(-1, 10) should throw Error', function() {
+			expect(() => getChange(-1, 10) ).to.throw(Error);
+	 	});
+
+	 	it('getChange(10, -200) should throw Error', function() {
+			expect(() => getChange(10, -200) ).to.throw(Error);
+	 	});
+
+	 	it('getChange("foo", [234]) should throw TypeError', function() {
+			expect(() => getChange("foo", [234]) ).to.throw(TypeError);
+	 	});
+ 	});
  });
 
 
